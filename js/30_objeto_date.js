c0bd4ff33e8486467ecbfc9950220da221c67a77
ocortez <theoceaan@gmail.com>
2016-09-26 22:11:57 -0300

    Curso JavaScript - Objeto Date #30

---
+++ js/30_objeto_date.js
@@ -0,0 +1,10 @@
+
+var data = new Date("Sep/26/2016 22:00:00");
+var dias = ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sabado"];
+alert(data);
+alert(data.getDay());
+alert(data.getHours());
+alert(data.getFullYear());
+alert(data.getUTCDate());
+alert(dias[data.getDay()]);
+
